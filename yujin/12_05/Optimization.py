b2837916a5ed29f7684bbca135daa8374613f9af
naongcode <naong4ha@gmail.com>
2024-12-11 01:57:14 +0900

    Update Optimization.py

--- yujin/12_05/Optimization.py
+++ yujin/12_05/Optimization.py
@@ -7,16 +7,18 @@ direction = input().split()
 
 #벌목한 횟수를 저장할 변수
 amount = 0
-
 growth = 0
 for i in range(Q):
+    # 벌목 조건 확인
     if (tree[location] + growth >= min_height):
         amount += tree[location] + growth
-        tree[location] = -growth  # 벌목한 나무는 현재 성장을 기준으로 초기화
+        tree[location] = 0  # 벌목 후 나무 값 0으로 설정
+    # 방향 이동
     if direction[i] == 'R':
         location = (location + 1) % length
     elif direction[i] == 'L':
         location = (location - 1) % length
+    # 성장 값 증가
     growth += 1
 	
 print(amount)
