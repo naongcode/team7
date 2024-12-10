ed8417215cc9662116d4cbf01043c0aac28ad4ee
naongcode <naong4ha@gmail.com>
2024-12-11 01:28:35 +0900

    Update wood.py

--- 은희-1205/wood.py
+++ 은희-1205/wood.py
@@ -10,16 +10,20 @@ D = list(input().split()) #움직이는 방향
 
 wood = 0 #벌목한 나무의 수
 
-for i in range(Q): #벌목횟수 동안 
-	if H[x-1] >= M: #만약 벌목 높이보다 벌목할 나무가 높다면
-		wood += H[x-1] #벌목한 나무의 수에 넣은 뒤
-		H[x-1] = 0 #벌목할 나무를 초기화한다.
+for i in range(Q):  # 벌목 횟수 동안
+    # 현재 나무의 최종 높이를 계산
+    current_height = H[x - 1] + total_increase
+
+    if current_height >= M:  # 만약 벌목 높이보다 나무가 높다면
+        wood += current_height  # 벌목한 나무의 수에 추가
+        H[x - 1] = -total_increase  # 벌목한 나무는 누적 높이를 제거한 상태로 0으로 만듦
+
 		
 	if D[i] == 'L':
 		x = (x-1) % N
 	elif D[i] == 'R':
 		x = (x+1) % N
 	
-	H = [h + 1 for h in H] #모든 나무의 높이를 1씩 증가(다음 벌목을 하기 위함임)
+	total_increase += 1 #모든 나무의 높이를 1씩 증가(다음 벌목을 하기 위함임)
 		
 print(wood)
